1a0a4872c44aaec45398c41198c3a16c6dcdb220
nuppy3 <nuppy3@hotmail.com>
2025-10-28 12:54:28 +0900

    Docusaurus対応: /docs 作成と/docs/src/pages/api.tsx作成

---
+++ docs/src/pages/api.tsx
@@ -0,0 +1,10 @@
+// @ts-expect-error: 型定義が存在しないため
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+export default function ApiPage() {
+  return (
+    <div style={{ height: '100vh' }}>
+      <SwaggerUI url="/swagger.json" />
+    </div>
+  );
+}
