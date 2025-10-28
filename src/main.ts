f1c02dec659caaad28f7c5ff1dd7797d52acea40
nuppy3 <nuppy3@hotmail.com>
2025-10-28 11:04:10 +0900

    nestjs/swagger対応：main.ts修正とswaggerパッケージインストール

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
 import { AppModule } from './app.module';
+// swqggrのAPIドキュメント生成用パッケージ
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // CORSの設定(FEからのアクセスを許可)
   app.enableCors();
   // app.enableCors({
@@ -13,7 +16,18 @@ async function bootstrap() {
   // });
   // nushi add: class validationをグローバルに適用
   app.useGlobalPipes(new ValidationPipe());
+
+  // 🔹 Swagger 設定
+  const config = new DocumentBuilder()
+    .setTitle('My API：Fleamarket API')
+    .setDescription('NestJS APIドキュメント(フリーマーケットAPI)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   // ポートを3000→4000に変更
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
