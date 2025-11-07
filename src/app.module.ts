09931d0f01a18ba53a6f12ad037ad6cfd85c9493
nuppy3 <nuppy3@hotmail.com>
2025-11-07 17:49:58 +0900

    store module initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ItemsNoDbModule } from './items-no-db/items-no-db.module';
 import { ItemsModule } from './items/items.module';
+import { StoresModule } from './stores/stores.module';
 import { TodoItemsModule } from './todo-items/todo-items.module';
 
 @Module({
@@ -9,7 +10,13 @@ import { TodoItemsModule } from './todo-items/todo-items.module';
   // 例：
   // nest.jsのlib（service)を使う場合もimportsに追加するだけ
   // providersに登録不要
-  imports: [ItemsModule, TodoItemsModule, ItemsNoDbModule, AuthModule],
+  imports: [
+    ItemsModule,
+    TodoItemsModule,
+    ItemsNoDbModule,
+    AuthModule,
+    StoresModule,
+  ],
   // コントローラー
   controllers: [],
   // DI対象
