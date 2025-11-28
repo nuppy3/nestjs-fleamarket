f5dab63bd9cf2ea8d59a6043bd33ae5bde34bffe
nuppy3 <nuppy3@hotmail.com>
2025-11-28 12:03:42 +0900

    Storesに認証機能を追加するためAuthModuleのimports

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,12 +1,15 @@
 /* istanbul ignore file */
 // ↑ これだけで Jest に「このファイルは無視して！」指示！！！
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StoresController } from './stores.controller';
 import { StoresService } from './stores.service';
 
 @Module({
-  imports: [PrismaModule],
+  // Prismaを使用するため追加 nushi add
+  // 認証を行うため、自作のAuthMoculeを追加
+  imports: [PrismaModule, AuthModule],
   controllers: [StoresController],
   providers: [StoresService],
 })
