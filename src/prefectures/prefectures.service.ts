417aeded6ac7c487cf1ec08ac995e5c1e48fcd99
nuppy3 <nuppy3@hotmail.com>
2025-12-08 18:28:22 +0900

    feat:prefecture createに一意制約エラーの実装追加

--- src/prefectures/prefectures.service.ts
+++ src/prefectures/prefectures.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 import { Prefecture } from '../prefectures/prefecture.model';
 import { PrismaService } from './../prisma/prisma.service';
 import { CreatePrefectureDto } from './dto/prefecture.dto';
@@ -23,7 +24,9 @@ export class PrefecturesService {
     return domains;
   }
 
-  async create(createPrefectureDto: CreatePrefectureDto) {
+  async create(
+    createPrefectureDto: CreatePrefectureDto,
+  ): Promise<Prefecture & { id: string }> {
     // dto取得
     const { name, code, kanaName, status, kanaEn } = createPrefectureDto;
 
@@ -32,12 +35,35 @@ export class PrefecturesService {
     // domain → prismaインプットパラメータ
     const prismaInput = { name, code, kanaName, status, kanaEn };
 
-    // prisma → domain
-    const created = await this.prismaService.prefecture.create({
-      data: prismaInput,
-    });
+    try {
+      // 都道府県情報の登録
+      const created = await this.prismaService.prefecture.create({
+        data: prismaInput,
+      });
 
-    return created;
+      // prisma → domain
+      // Prefecture TBL の項目は全てnot nullなので、Object.assign()で詰め替えているが
+      // 今後、任意項目が追加された場合は、任意項目の値がない場合、Prismaはnullを返すので、undefined
+      // に変換する処理が必要なので、Object.assign()ではなく、一つ一つ項目をセット、変換する。
+      const domain: Prefecture & { id: string } = Object.assign({}, created);
+
+      return domain;
+    } catch (e) {
+      // Prismaの既知のリクエストエラーであるかをチェック
+      // eはanyなので、instansof PrismaClientKnownRequestErrorでeの型ガードを行なっている
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === '2002') {
+          const field =
+            (e.meta?.target as string[])?.join(', ') || '不明なフィールド';
+          // 409 Conflictをスローし、コントローラーとNestJSのエラーハンドリング層でキャッチされる
+          throw new ConflictException(`指定された ${field} は既に存在します。`);
+        } else {
+          throw e;
+        }
+      } else {
+        throw e;
+      }
+    }
   }
 
   findOne(id: number) {
