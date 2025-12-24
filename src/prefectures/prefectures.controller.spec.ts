0b9705b6bee42639f468317063da09eed55863cc
nuppy3 <nuppy3@hotmail.com>
2025-12-24 10:34:30 +0900

    test:prefecture controller findAllの正常系テスト作成

--- src/prefectures/prefectures.controller.spec.ts
+++ src/prefectures/prefectures.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test } from '@nestjs/testing';
 import { PrefecturesService } from '../prefectures/prefectures.service';
+import { PrefectureResponseDto } from './dto/prefecture.dto';
 import { PrefecturesController } from './prefectures.controller';
+import { Prefecture } from './prefectures.model';
 
 const mockPrerectureService = {
   findAll: jest.fn(),
@@ -12,10 +14,10 @@ describe('■■■ Prefectures Controller TEST ■■■', () => {
   let prefecturesContorller: PrefecturesController; // テスト対象
   let prefecturesService: PrefecturesService; // Controllerから呼ばれるService
 
-  // servic mock data
-  // let mockPrefectures:  ()
-  // controller mock data(期待値)
-  //
+  // servic mock data(domain[])
+  let mockPrefectures: (Prefecture & { id: string })[];
+  // controller mock data(期待値:dto[])
+  let expectedPrefectureDtos: PrefectureResponseDto[];
 
   // テスト全体の前に1回だけ実行
   beforeAll(async () => {
@@ -43,8 +45,9 @@ describe('■■■ Prefectures Controller TEST ■■■', () => {
     );
     prefecturesService = module.get<PrefecturesService>(PrefecturesService);
     // service mock data
-
+    mockPrefectures = createSriviceMockData();
     // controller mock data(期待値)
+    expectedPrefectureDtos = createExpectedPrefectureDtos();
   });
 
   // 各テストケースの前に毎回実行：こっちでcreateTestingModule()してもいいが、
@@ -54,11 +57,27 @@ describe('■■■ Prefectures Controller TEST ■■■', () => {
     jest.clearAllMocks();
   });
 
+  //--------------------------------
+  // findAll()
+  //--------------------------------
   describe('findAll', () => {
-    it('正常系：dto配列(全項目)が返却される(dtoは全て@Expose()がセットされている', () => {});
+    it('正常系：dto配列(全項目)が返却される(dtoは全て@Expose()がセットされている', async () => {
+      // service modk data 作成
+      const domains: (Prefecture & { id: string })[] = mockPrefectures;
+      jest.spyOn(prefecturesService, 'findAll').mockResolvedValue(domains);
+
+      // テスト対象Controller呼び出し
+      const result = await prefecturesContorller.findAll();
+
+      // 検証
+      expect(result).toEqual(expectedPrefectureDtos);
+    });
     it('正常系：取得データが０件、dto[]の空配列が返却される', () => {});
   });
 
+  //--------------------------------
+  // create()
+  //--------------------------------
   describe('create', () => {
     it('正常系：都道府県情報作成（DTOの全項目あり）し、DTO全項目を返却する', () => {});
     it('正常系：都道府県情報作成（DTOの全項目あり）し、DTO全項目を返却する', () => {});
@@ -67,3 +86,135 @@ describe('■■■ Prefectures Controller TEST ■■■', () => {
     it('正常系：都道府県情報作成（DTOの全項目あり）し、DTO全項目を返却する', () => {});
   });
 });
+
+/**
+ * PrefectureServiceのMockデータを作成
+ * @returns
+ */
+function createSriviceMockData(): (Prefecture & { id: string })[] {
+  const domains: (Prefecture & { id: string })[] = [
+    {
+      id: '174d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '北海道',
+      code: '01',
+      kanaName: 'ホッカイドウ',
+      status: 'published',
+      kanaEn: 'hokkaido',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+    {
+      id: '274d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '青森',
+      code: '02',
+      kanaName: 'アオモリ',
+      status: 'published',
+      kanaEn: 'aomori',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+    {
+      id: '374d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '秋田',
+      code: '03',
+      kanaName: 'アキタ',
+      status: 'published',
+      kanaEn: 'akita',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+    {
+      id: '474d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '岩手',
+      code: '04',
+      kanaName: 'イワテ',
+      status: 'published',
+      kanaEn: 'iwate',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+    {
+      id: '574d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '山形',
+      code: '05',
+      kanaName: 'ヤマガタ',
+      status: 'published',
+      kanaEn: 'yamagata',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+    {
+      id: '674d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '東京都',
+      code: '13',
+      kanaName: 'トウキョウト',
+      status: 'published',
+      kanaEn: 'tokyo-to',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+    },
+  ];
+
+  return domains;
+}
+
+function createExpectedPrefectureDtos(): PrefectureResponseDto[] {
+  const dtos: PrefectureResponseDto[] = [
+    {
+      id: '174d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '北海道',
+      code: '01',
+      kanaName: 'ホッカイドウ',
+      status: 'published',
+      kanaEn: 'hokkaido',
+      statusLabel: '反映中',
+    },
+    {
+      id: '274d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '青森',
+      code: '02',
+      kanaName: 'アオモリ',
+      status: 'published',
+      kanaEn: 'aomori',
+      statusLabel: '反映中',
+    },
+    {
+      id: '374d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '秋田',
+      code: '03',
+      kanaName: 'アキタ',
+      status: 'published',
+      kanaEn: 'akita',
+      statusLabel: '反映中',
+    },
+    {
+      id: '474d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '岩手',
+      code: '04',
+      kanaName: 'イワテ',
+      status: 'published',
+      kanaEn: 'iwate',
+      statusLabel: '反映中',
+    },
+    {
+      id: '574d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '山形',
+      code: '05',
+      kanaName: 'ヤマガタ',
+      status: 'published',
+      kanaEn: 'yamagata',
+      statusLabel: '反映中',
+    },
+    {
+      id: '674d2683-7012-462c-b7d0-7e452ba0f1ab',
+      name: '東京都',
+      code: '13',
+      kanaName: 'トウキョウト',
+      status: 'published',
+      kanaEn: 'tokyo-to',
+      statusLabel: '反映中',
+    },
+  ];
+
+  return dtos;
+}
