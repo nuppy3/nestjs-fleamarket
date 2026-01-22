378c149012a4c0a5fd968b6a04366c25080c243e
nuppy3 <nuppy3@hotmail.com>
2026-01-22 13:24:29 +0900

    feat:SI221 region factory 実装

--- src/regions/domain/regions.factory.ts
+++ src/regions/domain/regions.factory.ts
@@ -1,1 +1,16 @@
-export class RegionsFactory {}
+import { CreateRegionDto } from '../dto/region.dto';
+import { Region } from './regions.model';
+
+/**
+ * RegionドメインのFactory
+ * dto → domain
+ */
+export class RegionFactory {
+  static fromCreateDto(dto: CreateRegionDto) {
+    // TODO
+    // 将来的には、Region.createNew()を用意して、Region.createNewを呼び出すだけにする
+    // その際、RegionドメインのcreateNew()の中だけでnew Region()できるようにする。
+    // 他からnew Region()できないように。
+    return new Region(dto.code, dto.name, dto.kanaName, dto.status, dto.kanaEn);
+  }
+}
