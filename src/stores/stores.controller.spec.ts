d11a963b9899e3201aa84d036229cb14911dc1b6
nuppy3 <nuppy3@hotmail.com>
2026-02-05 10:21:50 +0900

    test:SI206 Coding the UT code of the controller

--- src/stores/stores.controller.spec.ts
+++ src/stores/stores.controller.spec.ts
@@ -126,14 +126,54 @@ describe('StoresController TEST', () => {
         });
       });
 
-      it('正常系(4)：XXXXXを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
+      it('正常系(4)：regionCodeを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
         // ServiceのMockデータを作成：中身は適当
         jest.spyOn(storesService, 'findAll').mockResolvedValue(mockStores);
 
         // テスト対象Controller呼び出し:適当なクエリを渡す
         // 引数: 一応意味のあるモノにしたが、中身は適当でよい
         const query: FindAllStoresQueryDto = {
-          status: 'published',
+          regionCode: '03',
+        };
+        await storesController.findAll(query);
+
+        // 検証: Serviceを期待通りの引数で呼んでいるか → store.controler内で、特にqueryを
+        // 変換処理せずにserviceに渡してるので、当該試験はあまり意味がないが、一応
+        // filterのテストは以下の結果検証は不要
+        // expect(result).toEqual(expectedStoreDto);
+        expect(jest.spyOn(storesService, 'findAll')).toHaveBeenCalledWith(
+          query,
+        );
+      });
+
+      it('正常系(5)：XXXXXを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
+        // ServiceのMockデータを作成：中身は適当
+        jest.spyOn(storesService, 'findAll').mockResolvedValue(mockStores);
+
+        // テスト対象Controller呼び出し:適当なクエリを渡す
+        // 引数: 一応意味のあるモノにしたが、中身は適当でよい
+        const query: FindAllStoresQueryDto = {
+          regionCode: '03',
+        };
+        await storesController.findAll(query);
+
+        // 検証: Serviceを期待通りの引数で呼んでいるか → store.controler内で、特にqueryを
+        // 変換処理せずにserviceに渡してるので、当該試験はあまり意味がないが、一応
+        // filterのテストは以下の結果検証は不要
+        // expect(result).toEqual(expectedStoreDto);
+        expect(jest.spyOn(storesService, 'findAll')).toHaveBeenCalledWith(
+          query,
+        );
+      });
+
+      it('正常系(6)：XXXXXを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
+        // ServiceのMockデータを作成：中身は適当
+        jest.spyOn(storesService, 'findAll').mockResolvedValue(mockStores);
+
+        // テスト対象Controller呼び出し:適当なクエリを渡す
+        // 引数: 一応意味のあるモノにしたが、中身は適当でよい
+        const query: FindAllStoresQueryDto = {
+          regionCode: '03',
         };
         await storesController.findAll(query);
 
@@ -150,7 +190,7 @@ describe('StoresController TEST', () => {
     describe('findAllの絞り込み(filter) 複合条件のテスト', () => {
       // queryの変換処理はcontrolerで実施していないし、
       // controllerで複合ケースの試験は不要な気もするが一応
-      it('(1)+(2)+(3) 都道府県コード＋statusを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
+      it('(1)+(2)+(3)+(4) 都道府県コード/status/name/エリアコードを指定した場合、Serviceを期待通りの引数で呼んでいるか', async () => {
         // ServiceのMockデータを作成
         jest.spyOn(storesService, 'findAll').mockResolvedValue(mockStores);
 
@@ -160,6 +200,7 @@ describe('StoresController TEST', () => {
           prefectureCode: '13',
           status: 'published',
           name: '赤羽',
+          regionCode: '03',
         };
         await storesController.findAll(query);
 
@@ -169,6 +210,7 @@ describe('StoresController TEST', () => {
           prefectureCode: '13',
           status: 'published',
           name: '赤羽',
+          regionCode: '03',
         });
       });
     });
