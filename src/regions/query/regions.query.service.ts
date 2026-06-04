830be3e5318aee3ea10c480867586895cb614c36
nuppy3 <nuppy3@hotmail.com>
2026-06-04 12:52:14 +0900

    feat:SI453 coded the call to Query service in the regions.controller.ts

--- src/regions/query/regions.query.service.ts
+++ src/regions/query/regions.query.service.ts
@@ -26,40 +26,42 @@ export class RegionsQueryServiceService {
       orderBy: { code: 'asc' },
     });
 
+    // prisma[] → dto[] の 変換ロジック
     // ⭐️UIを意識して、データ変換などが必要になった際は以下のようなロジックがいい感じ
-    // もしロジックが必要になったら、以下のコメントを解除
-    // コメント --- ここから ---
-    // const dtos = regions.map((prismaRegion, index) => {
-    //   const plainObj = {
-    //     id: prismaRegion.id,
-    //     name: prismaRegion.name,
-    //     code: prismaRegion.code,
-    //     kanaName: prismaRegion.kanaName ?? undefined, // 例: nullならundefined
-    //     status: prismaRegion.status,
-    //     kanaEn: prismaRegion.kanaEn,
-    //     sortOrder: index + 1, // 例: 連番を画面用に付与
-    //   };
+    const dtos = regions.map((prismaRegion) => {
+      // データ変換
+      const plainObj = {
+        id: prismaRegion.id,
+        name: prismaRegion.name,
+        code: prismaRegion.code,
+        // kanaName: prismaRegion.kanaName ?? undefined, // 例: nullならundefined
+        kanaName: prismaRegion.kanaName,
+        status: prismaRegion.status,
+        kanaEn: prismaRegion.kanaEn,
+        prefectureCount: prismaRegion._count.prefectures,
+        // sortOrder: index + 1, // 例: 連番を画面用に付与
+      } satisfies Partial<RegionResponseDto>;
 
-    //   return instanceToPlain(
-    //     plainToInstance(RegionResponseDto, plainObj, {
-    //       // @Expose() がないプロパティは全部消える
-    //       // 値が undefined or null の場合、キーごと消える
-    //       excludeExtraneousValues: true,
-    //     }),
-    //   ) as RegionResponseDto;
-    // });
+      // prisma[] → dto[]
+      return instanceToPlain(
+        plainToInstance(RegionResponseDto, plainObj, {
+          // @Expose() がないプロパティは全部消える
+          // 値が undefined or null の場合、キーごと消える
+          excludeExtraneousValues: true,
+        }),
+      ) as RegionResponseDto;
+    });
 
-    // return dtos;
-    // コメント --- ここまで ---
+    return dtos;
 
     // prisma[] → dto[]
     // memo: 元々controller()で実装していたdto変換処理をそのまま
-    return instanceToPlain(
-      plainToInstance(RegionResponseDto, regions, {
-        // @Expose() がないプロパティは全部消える
-        // 値が undefined or null の場合、キーごと消える
-        excludeExtraneousValues: true,
-      }),
-    ) as RegionResponseDto[];
+    // return instanceToPlain(
+    //   plainToInstance(RegionResponseDto, regions, {
+    //     // @Expose() がないプロパティは全部消える
+    //     // 値が undefined or null の場合、キーごと消える
+    //     excludeExtraneousValues: true,
+    //   }),
+    // ) as RegionResponseDto[];
   }
 }
