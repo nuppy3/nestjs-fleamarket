349d700be1b036c0481778006dc4f0bc0ab9de0f
nuppy3 <nuppy3@hotmail.com>
2026-06-13 19:18:54 +0900

    test:SI474 coded the test code of the region model in the regions.model.spec.ts

--- src/regions/domain/regions.model.spec.ts
+++ src/regions/domain/regions.model.spec.ts
@@ -1,4 +1,5 @@
 import {
+  RegionAlreadyEditedException,
   RegionAlreadyPublishedException,
   RegionAlreadySuspendedException,
 } from './errors/regions.exceptions';
@@ -147,6 +148,69 @@ describe('□□□ Region Domain Test □□□', () => {
       // messageの検証
       expect(() => region.publish()).toThrow(
         `この地域は掲載状態のため、更新できません。(編集中/停止中のみ更新可) 地域： ${region.name}`,
+      );
+    });
+  });
+
+  // -----------------------------
+  // unpublish() test
+  // -----------------------------
+  describe('------ unpublish() test ------', () => {
+    it('正常系: validateCanUnpublishを通過し、statusとupdatedAtが更新されること', () => {
+      // Region domain 生成
+      const region = Region.reconstitute({
+        name: '北海道',
+        code: '01',
+        kanaName: 'ほっかいどう',
+        // 掲載中
+        status: 'published',
+        kanaEn: 'hokkaidou',
+        createdAt: new Date('2025-04-05T10:00:00.000Z'),
+        updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      }) satisfies ReconstituteRegionProps;
+
+      // 実行直前の時刻
+      const before = new Date();
+
+      // test対象のfunction呼び出し
+      region.unpublish();
+
+      // 検証
+      expect(region.status).toBe(RegionStatus.EDITING);
+      // 日付、時刻の検証について、誤差を認める方法
+      // 「1秒以内に実行されたか」という検証で、Jestの toBeGreaterThan（より大きい）などを使い
+      // 実行後の時刻が、実行前よりも未来であることを確認するやり方があるようです。
+      expect(region.updatedAt.getTime()).toBeGreaterThanOrEqual(
+        before.getTime(),
+      );
+    });
+
+    it('異常系: 削除対象のRegionが既に編集中の場合、Errorをスローする', () => {
+      const region = Region.reconstitute({
+        name: '北海道',
+        code: '01',
+        kanaName: 'ほっかいどう',
+        status: RegionStatus.EDITING, // '編集中'
+        kanaEn: 'hokkaidou',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      // ✅ 実行せずに「リモコン（アロー関数）」だけを渡す
+      // 前に region.remove() を単体で書かないこと！
+      // region.remove(): 「今すぐ爆弾を起動しろ！」
+      // () => region.remove(): 「爆弾を起動するためのリモコンです。どうぞ」
+      // expect(() => region.remove()).toThrow(
+      //   `この地域はすでに利用停止状態です。地域： ${region.name}`,
+      // );
+
+      // DomainExceptionの検証
+      expect(() => region.unpublish()).toThrow(
+        new RegionAlreadyEditedException(region.name),
+      );
+      // messageの検証
+      expect(() => region.unpublish()).toThrow(
+        `この地域は既に編集中のため、更新できません。 地域： 北海道`,
       );
     });
   });
