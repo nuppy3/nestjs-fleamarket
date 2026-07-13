61b3c847667fa67a7ce62e4a3661d8bfa80005bf
nuppy3 <nuppy3@hotmail.com>
2026-07-13 10:58:48 +0900

    test:SI521 coded the test code in the stroes.service.spec.ts

--- src/stores/stores.service.spec.ts
+++ src/stores/stores.service.spec.ts
@@ -13,6 +13,7 @@ import { PrefecturesService } from '../prefectures/prefectures.service';
 import { RegionsModule } from '../regions/regions.module';
 import { PublishStoreDto } from './dto/publish-store.dto';
 import { CreateStoreDto } from './dto/store.dto';
+import { StoreReadModel } from './query/stores.read-model';
 import { SortOrder, Store, StoreFilter, StoreStatus } from './stores.model';
 import { StoresService } from './stores.service';
 
@@ -122,7 +123,7 @@ describe('StoresService Test', () => {
       const result = await storesService.findAll();
 
       // 結果検証
-      expect(result).toEqual(expectedStores);
+      expect(result).toEqual(createExpectStoresHaveReadModel());
 
       // 念の為、where句も検証、orderByはデフォルトソート指定を検証
       expect(jest.spyOn(prismaService.store, 'findMany')).toHaveBeenCalledWith({
@@ -1861,6 +1862,102 @@ function createMockStores(): (StorePrisma & {
 }
 
 // 期待値作成：serviceの返却値
+function createExpectStoresHaveReadModel(): PaginatedResult<StoreReadModel> {
+  const stores: StoreReadModel[] = [
+    {
+      id: 'b74d2683-7012-462c-b7d0-7e452ba0f1ab',
+      code: '00001',
+      name: '山田電気 赤羽店',
+      status: 'published',
+      email: 'yamada-akabane@test.co.jp',
+      phoneNumber: '03-1122-9901',
+      kanaName: 'ﾔﾏﾀﾞﾃﾞﾝｷ ｱｶﾊﾞﾈｼﾃﾝ',
+      // prefecture: '東京都',
+      holidays: ['WEDNESDAY', 'SUNDAY'],
+      zipCode: '100-0001',
+      address: '東京都北区赤羽３丁目',
+      businessHours: '10:00-20:00',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      userId: '633931d5-2b25-45f1-8006-c137af49e53d',
+      prefecture: {
+        name: '東京都',
+        code: '13',
+        kanaName: 'トウキョウト',
+        status: 'published',
+        kanaEn: 'tokyo-to',
+        // createdAt: new Date('2025-04-05T10:00:00.000Z'),
+        // updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      },
+    },
+    {
+      id: '70299537-4f16-435f-81ed-7bed4ae63758',
+      code: '00002',
+      name: '山田電気 江戸川店',
+      status: 'published',
+      email: 'yamada-akabane@test.co.jp',
+      phoneNumber: '03-1122-9901',
+      kanaName: 'ﾔﾏﾀﾞﾃﾞﾝｷ ｴﾄﾞｶﾞﾜｼﾃﾝ',
+      // prefecture: '東京都',
+      holidays: ['WEDNESDAY', 'SUNDAY'],
+      zipCode: '100-0001',
+      address: '東京都江戸川区西念1丁目10番地',
+      businessHours: '10:00-20:00',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      userId: '633931d5-2b25-45f1-8006-c137af49e53d',
+      prefecture: {
+        name: '東京都',
+        code: '13',
+        kanaName: 'トウキョウト',
+        status: 'published',
+        kanaEn: 'tokyo-to',
+        // createdAt: new Date('2025-04-05T10:00:00.000Z'),
+        // updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      },
+    },
+    {
+      id: '1dfe32a5-ddac-4f3c-ad16-98e48a4dd63d',
+      code: '00003',
+      name: '山田電気 銀座店',
+      status: 'published',
+      email: 'yamada-akabane@test.co.jp',
+      phoneNumber: '03-1122-9901',
+      kanaName: 'ﾔﾏﾀﾞﾃﾞﾝｷ ｷﾞﾝｻﾞｼﾃﾝ',
+      // prefecture: '東京都',
+      holidays: ['WEDNESDAY', 'SUNDAY'],
+      zipCode: '100-0001',
+      address: '東京都中央区西銀座5丁目',
+      businessHours: '10:00-20:00',
+      createdAt: new Date('2025-04-05T10:00:00.000Z'),
+      updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      userId: '633931d5-2b25-45f1-8006-c137af49e53d',
+      prefecture: {
+        name: '東京都',
+        code: '13',
+        kanaName: 'トウキョウト',
+        status: 'published',
+        kanaEn: 'tokyo-to',
+        // createdAt: new Date('2025-04-05T10:00:00.000Z'),
+        // updatedAt: new Date('2025-04-05T12:30:00.000Z'),
+      },
+    },
+  ];
+
+  // ページネーションされたStoreドメインオブジェクト
+  const paginatedResult: PaginatedResult<StoreReadModel> = {
+    data: stores,
+    meta: {
+      totalCount: 3,
+      page: 1,
+      size: 20,
+    },
+  };
+
+  return paginatedResult;
+}
+
+// 期待値作成：serviceの返却値: TODO いづれ、上記のfunctionに置き換える
 function createExpectStores(): PaginatedResult<Store & { id: string }> {
   const stores: (Store & { id: string })[] = [
     {
