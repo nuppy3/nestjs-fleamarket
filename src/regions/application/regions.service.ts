349df23696daa2096abcf2b17a7ff44dc477c0c2
nuppy3 <nuppy3@hotmail.com>
2026-08-01 21:41:04 +0900

    feat:SI549 added the regionDetailReadModel

--- src/regions/application/regions.service.ts
+++ src/regions/application/regions.service.ts
@@ -21,7 +21,8 @@ export class RegionsService {
   ) {}
 
   /**
-   * エリア情報取得（全て）
+   * エリア情報取得（全て）:
+   * memo 当findAllはどこからも呼ばれていない。Query ServiceにfindAll()を移動している。
    */
   async findAll(): Promise<(Region & { id: string })[]> {
     // エリア情報取得
---
+++ src/regions/query/region-detail.read-model.ts
@@ -0,0 +1,19 @@
+import { RegionStatus } from '../domain/regions.model';
+
+/**
+ * 画面/APIに都合よく整形された、読み取り専用のModel
+ *
+ * 用途：Query ServiceにてDBからのデータを当Modelに詰め替えてControllerへ返却する。
+ * メモ：Domainとたまたま同等のプロパティだったとしても(将来変わる可能性もあるし）、Domainルールは
+ *.     流出させたくないので、Read Modelに詰め替えるのがBPとされる。
+ */
+export type regionDetailReadModel = {
+  id: string;
+  code: string;
+  name: string;
+  kanaName: string;
+  kanaEn: string;
+  status: RegionStatus;
+  createdAt: Date;
+  updatedAt: Date;
+};
