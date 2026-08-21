0a94965515a023bf63a86ce226f8046e8c75ee70
nuppy3 <36092159+nuppy3@users.noreply.github.com>
2026-08-21 15:05:46 +0900

    feat: エリア一覧取得APIにFindAllRegionsQueryDto(エリアコードフィルタ)を追加
    - FindAllRegionsQueryDtoにエリアコード(code)フィールドを追加、バリデーション実装
    - RegionsController.findAll()で@Query()によりFindAllRegionsQueryDtoを受け取れるよう変更
    
    Jira: SI-583

--- src/regions/dto/region.dto.ts
+++ src/regions/dto/region.dto.ts
@@ -1,9 +1,26 @@
 import { Expose, Type } from 'class-transformer';
-import { IsEnum, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 import { PaginatedResult } from '../../common/interfaces/paginated-result.interface';
 import { PrefectureStatus } from '../../prefectures/prefectures.model';
 import { PaginationMetaDto } from '../../stores/dto/store.dto';
 import { Region, RegionStatus } from '../domain/regions.model';
+
+/**
+ * エリア情報検索クエリDTO(フィルター/ソート順などのクエリパラメータDTO)
+ */
+export class FindAllRegionsQueryDto {
+  // エリアコード
+  @IsOptional() // 任意項目デコレーター(渡された値がnullの場合は、以降のIsString、MaxLengthなどを無視する)
+  @IsString() // 任意項目だが入力された際のValidation
+  @MaxLength(2) // 任意項目だが入力された際のValidation
+  code?: string;
+}
 
 /**
  * CreateRegionDto
