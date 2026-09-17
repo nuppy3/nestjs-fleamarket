273038b5e3d1c696ee8bb985e3913caf78a08700
nuppy3 <nuppy3@hotmail.com>
2026-09-17 10:32:57 +0900

    feat:SI688 added the type of UpdateRegionCommand for regions service

--- src/regions/application/commands/update-region.command.ts
+++ src/regions/application/commands/update-region.command.ts
@@ -1,3 +1,20 @@
 import { RegionState } from 'src/regions/domain/regions.model';
 
-export type UpdateRegionCommand = Omit<RegionState, ''>;
+/**
+ * 【更新用】
+ * Region domain更新時に必要なプロパティを型として定義
+ * 日付以外は更新対象
+ *
+ * 型はRegionStateをベースとした任意項目：
+ *  ・Partial<Omit<RegionState, '' | '' | ''>>
+ *
+ * memo:現状、regions.model.ts(domain)のUpdateRegionPropsと全く同じ型定義となっている。
+ * これは、どちらもdomain作成用のEntityの型なので、DomainのStateをベースに作成しているので
+ * 自然な現象。「レイヤー境界の翻訳コスト」として想定内の現象。
+ *
+ * ⭐️依存方向的には Application → Domain の依存は許されるので、当該Comanndは作成せずにDomain
+ * のUpdateRegionPropsを使ってもいいが、厳密なDDDを体感するために当該typeを定義している。
+ */
+export type UpdateRegionCommand = Partial<
+  Omit<RegionState, 'updatedAt' | 'createdAt'>
+>;
