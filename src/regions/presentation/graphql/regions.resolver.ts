83f0ef8a28d73103a89cea75c564a4d6f389fce0
nuppy3 <nuppy3@hotmail.com>
2026-09-24 16:03:26 +0900

    feat:SI695 implemented the regions for Resolver and gql initial commit

--- src/regions/presentation/graphql/regions.resolver.ts
+++ src/regions/presentation/graphql/regions.resolver.ts
@@ -1,4 +1,29 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { RegionsQueryService } from 'src/regions/query/regions.query.service';
+import { RegionObjectType } from './object-types/region.object-type';
 
 @Resolver()
-export class RegionsResolver {}
+export class RegionsResolver {
+  constructor(private readonly queryService: RegionsQueryService) {}
+
+  /**
+   * regions
+   *
+   * @returns
+   */
+  @Query(() => [RegionObjectType])
+  async regions(): Promise<RegionObjectType[]> {
+    const readModel = await this.queryService.findAll({});
+    return [
+      {
+        id: '',
+        name: '',
+        code: '',
+        kanaName: '',
+        kanaEn: '',
+        status: 'editing',
+        statusLabel: '',
+      } satisfies RegionObjectType,
+    ];
+  }
+}
